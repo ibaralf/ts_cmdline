cb25b6664540b9a6e2ad3b50962e49ca03b7bc3d
Ibarra Alfonso <ibarraalfonso@FormationMac.local>
2025-03-25 11:37:05 -0700

    added func for user multiple choice

--- src/cmdline.ts
+++ src/cmdline.ts
@@ -3,6 +3,10 @@ import * as fs from 'fs';
 import readConfigFile from './config_lib';
 import {Config} from './config_lib';
 
+// TODO:
+// * add date entry
+// * add multiple choice
+// * add range options
 let cfg = <Config>{};
 
 async function getUserInput(query: string): Promise<string> {
@@ -19,7 +23,6 @@ async function getUserInput(query: string): Promise<string> {
     });
 }
 
-// TODO: add range options
 async function getInteger(userPrompt? :string): Promise<number> {
     const intPattern: RegExp = /^[0-9]+$/i;
     var promptText = ""
@@ -31,9 +34,6 @@ async function getInteger(userPrompt? :string): Promise<number> {
     while(notDone) {
         let myInteger: number = null;
         const intval = await getUserInput(promptText);
-        console.log(`YOU Entered: ${intval}`)
-        //myInteger = parseInt(intval);
-        //if (!isNaN(myInteger) && Number.isInteger(myInteger)) {
         if (intPattern.test(intval)) {
             notDone = false;
             myVal = parseInt(intval);
@@ -72,6 +72,50 @@ async function promptYesNo(userPrompt? :string): Promise<string> {
     });
 }
 
+function errorPromise(): Promise<string> {
+    return new Promise((resolve) => {
+        resolve('ERROR')
+    });
+}
+
+async function selectChoice(choices: string[], choiceOpt?: string[], userPrompt? :string): Promise<string> {
+
+    var userChoices: string[] = [];
+    if (choiceOpt) {
+        if (choices.length != choiceOpt.length) {
+            return errorPromise();
+        }
+        userChoices = choiceOpt
+    } else {
+        choices.forEach((choice, index) => {
+            userChoices.push(String(index + 1));
+        });
+    }
+    var promptText = ""
+    if (userPrompt) {
+        promptText = userPrompt + " ";
+    }
+    choices.forEach((choice, index) => {
+        console.log(userChoices[index] + ") " + choice);
+    });
+    var myVal: string = null;
+    var notDone = true;
+    while(notDone) {
+        let myChoice: string = null;
+        const choiceVal = await getUserInput(promptText);
+        if (userChoices.includes(choiceVal)) {
+            notDone = false;
+            myVal = choiceVal;
+        } else {
+            console.log('ERROR: Select one [' + userChoices + "]");
+        }
+    }
+
+    return new Promise((resolve) => {
+        resolve(myVal)
+    });
+}
+
 async function showLogo(): Promise<void> {
     let lpath = process.cwd() + "/" + cfg.logo_path;
     const logoText = await fs.promises.readFile(lpath, 'utf8');
@@ -83,6 +127,11 @@ async function main() {
     cfg = await readConfigFile()
     
     const nval = await Promise.all([showLogo()])
+
+    const picks: string[] = ["Apple", "Banana", "Carrot", "All"];
+    const pickOpt: string[] = ["a", "b", "c", "d"];
+    const pile = await selectChoice(picks, pickOpt, "What is your favorite?");
+    console.log(`YOU SELECTED: ${pile}`)
 
     //console.log('FIRST');
     const name = await getUserInput('Please enter your name: ');
